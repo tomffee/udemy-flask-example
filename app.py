9f9244822640a966397864ab15b772c4b5b8e68a
Tomas Patoprsty <tomffee@gmail.com>
2017-03-27 15:24:33 +0200

    Handling db

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+import os
+
+from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
@@ -8,15 +10,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -29,4 +29,10 @@ api.add_resource(StoreList, '/stores')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(debug=True)  # important to mention debug=True
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
